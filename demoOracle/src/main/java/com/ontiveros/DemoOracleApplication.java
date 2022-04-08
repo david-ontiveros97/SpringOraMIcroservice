e5c460cf142b368a2c1f35ce08a1405e9f578408
david-ontiveros97 <david.oivd911@gmail.com>
2022-04-07 23:38:13 -0500

    Commit inicial de proyecto.

---
+++ demoOracle/src/main/java/com/ontiveros/DemoOracleApplication.java
@@ -0,0 +1,13 @@
+package com.ontiveros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOracleApplication.class, args);
+	}
+
+}
---
+++ demoOracle/src/test/java/com/ontiveros/DemoOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontiveros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
